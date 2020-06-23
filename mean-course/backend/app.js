32361a5b765af4b7190d56e0469d0f790bcafcb7
Karan Verma <karanv598@gmail.com>
2020-06-23 22:21:35 +0530

    Fetching Initial posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first')
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {id: 'sdkjfhdjs', title: 'First server side post', content: 'first content coming from server'},
+        {id: 'sdkjfhddd', title: 'Second server side post', content: 'second content coming from server'},
+    ];
+    return res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts
+    });
     next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!')
 });
 
 module.exports = app;
