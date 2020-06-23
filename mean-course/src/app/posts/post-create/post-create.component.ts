8ab927a222afa5f1696035144458d9f1f27d2c74
Karan Verma <karanv598@gmail.com>
2020-06-23 20:28:02 +0530

    Setting up UI of project by using Components, EventEmitters, Models, Angular Material

---
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+    enteredTitle = '';
+    enteredContent = '';
+    @Output() postCreated = new EventEmitter();
+    
+    onAddPost() {
+        const post: Post = {
+            title: this.enteredTitle, content: this.enteredContent
+        };
+        this.postCreated.emit(post)
+    }
+}
