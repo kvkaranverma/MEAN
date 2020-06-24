f2ba360355f2c14cade47212b3cce8328d83dd67
Karan Verma <karanv598@gmail.com>
2020-06-24 20:33:52 +0530

    Updating front end after deleting posts

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -45,7 +45,9 @@ export class PostsService {
     deletePost(postId: string) {
         this.http.delete('http://localhost:3000/api/posts/'+postId)
             .subscribe(() => {
-                console.log('Deleted');
+                const updatedPosts = this.posts.filter(post => postId !== post.id);
+                this.posts = updatedPosts;
+                this.postsUpdated.next([...this.posts]);
             })
     }
 }
