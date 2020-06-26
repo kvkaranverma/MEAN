1ab6fa17cd20a7561d3bdc7ce9f1d3ca81da8c16
Karan Verma <karanv598@gmail.com>
2020-06-26 10:40:54 +0530

    Working on pagination backend

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -45,7 +45,15 @@ router.post('/api/posts', multer({storage: storage}).single('image'), async (req
 });
 
 router.get('/api/posts', async (req, res) => {
-    await Post.find()
+    const pageSize = +req.query.pageSize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    if(pageSize && currentPage) {
+        postQuery.skip(pageSize * (currentPage - 1))
+            .limit(pageSize);
+    }
+
+    await postQuery
         .then(posts => {
             res.status(200).json({
                 message: 'Posts fetched successfully!',
