c386a68da0ce974fa58a1343559373cf3df828a4
Karan Verma <karanv598@gmail.com>
2020-06-26 18:55:49 +0530

    Implemention SPA authentication and using JWT token

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 
 const User = require('../models/user');
+const user = require('../models/user');
 
 router.post('/signup', (req, res) => {
     bcrypt.hash(req.body.password, 9)
@@ -25,6 +27,33 @@ router.post('/signup', (req, res) => {
                 });
         }).catch(err => {
             console.log(err)
+        })
+})
+
+router.post('/login', (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(!user) {
+                return res.status(404).send({
+                    message: 'Auth failed'
+                })
+            }
+            return bcrypt.compare(req.body.password, user.password);
+        }).then(result => {
+            if(!result) {
+                return res.status(404).send({
+                    message: 'Auth failed'
+                })
+            }
+            const token = jwt.sign(
+                {email: user.email, userId: user._id},
+                'this_is_secret_fuck_off',
+                {expiresIn: '1h'});
+
+        }).catch(err => {
+            return res.status(404).send({
+                message: 'Auth failed'
+            })
         })
 })
 
