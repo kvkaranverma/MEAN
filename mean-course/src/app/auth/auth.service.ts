54b974eaa7c27ffccd6badb1a28d12bd2a2f14f3
Karan Verma <karanv598@gmail.com>
2020-06-27 18:04:11 +0530

    using global angular config

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -4,6 +4,10 @@ import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 
+import { environment } from "../../environments/environment";
+
+const BACKEND_URL = environment.apiUrl+ '/user';
+
 @Injectable({providedIn: 'root'})
 export class AuthService {
     private userId: string;
@@ -32,7 +36,7 @@ export class AuthService {
 
     createUser(email: string, password: string) {
         const authData: AuthData = {email, password};
-        return this.http.post('http://localhost:3000/api/user/signup', authData)
+        return this.http.post(BACKEND_URL+'/signup', authData)
             .subscribe(response => {
                 this.router.navigate['/'];
             }, error => {
@@ -43,7 +47,7 @@ export class AuthService {
     loginUser(email: string, password: string) {
         const authData: AuthData = {email, password};
         console.log(authData)
-        this.http.post<{token: string, expiresIn: number, userId: string}>('http://localhost:3000/api/user/login', authData)
+        this.http.post<{token: string, expiresIn: number, userId: string}>(BACKEND_URL+'/login', authData)
             .subscribe(response => {
                 const token  = response.token;
                 this.token = token;
